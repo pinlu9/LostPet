c58dfe2043a7ff2451c7e0a5f87322a84cf690e5
Pin <pinlu9@gmail.com>
2015-11-29 13:52:40 -0500

    save

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,7 +2,7 @@ class Pet < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :pet_img, :styles => { :pet_index => "180x180>", :pet_show => "300x300>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :pet_img, :styles => { :pet_index => "180x180>", :pet_show => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :pet_img, :content_type => /\Aimage\/.*\Z/
 
 end
